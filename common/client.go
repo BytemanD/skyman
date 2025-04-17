d02a72a77061ee8a01c0ce8c016196592ca610a4
BytemanD <byteman.e@outlook.com>
2025-04-17 16:45:26 +0800

    fix: 修复version命令空指针异常
    Signed-off-by: BytemanD <byteman.e@outlook.com>

--- common/client.go
+++ common/client.go
@@ -7,9 +7,9 @@ import (
 
 func DefaultClient() *openstack.Openstack {
 	conn, err := openstack.Connect()
-	conn.SetRetryCount(CONF.RetryCount)
 	if err != nil {
 		console.Fatal("connect cloud failed: %s", err)
 	}
+	conn.SetRetryCount(CONF.RetryCount)
 	return conn
 }
